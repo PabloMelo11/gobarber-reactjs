51c04ffe597ad50669ca4919a157393d54df56cc
Pablo Melo <pablomelodev@gmail.com>
2020-05-02 00:57:37 -0300

    Tool error suggestion

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tolltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,18 +23,32 @@ export const Container = styled.div<ContainerProps>`
     margin-top: 8px;
   }
 
-  ${props =>
-    props.isFocused &&
-    css`
-      color: #ff9000;
-      border-color: #ff9000;
-    `}
+    ${props =>
+      props.isErrored &&
+      css`
+        border-color: #c53030;
+      `}
+
+      ${props =>
+        props.isErrored &&
+        css`
+          border-color: #c53030;
+        `}
+
+        ${props =>
+          props.isFocused &&
+          css`
+            color: #ff9000;
+            border-color: #ff9000;
+          `}
 
   ${props =>
     props.isFilled &&
     css`
       color: #ff9000;
     `}
+
+
 
   input {
     flex: 1;
@@ -49,5 +66,23 @@ export const Container = styled.div<ContainerProps>`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+  }
+
+  &::before {
+    border: #c53030 transparent;
   }
 `;
